dfe85d0a689fd4a9da2bdb42e78db94919b9d44d
BenLiu <liumanpan1990@hotmail.com>
2022-09-23 15:54:59 -0400

    add function access local storage

--- script.js
+++ script.js
@@ -1,44 +1,84 @@
+//declare time variable by using moment API
+let today = moment().format('YYYY-MM-DD');
+let now = moment().format("HH");
 
+//declare event list in calendar
+let calendarList = [];
+
+// timer to display time and update calender color every second.
+setInterval(function () {
+    displayTime();
+    updateBgColor();
+}
+    , 1000);
+
+//fuction to render time
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     $("#currentDay").text(rightNow);
 }
 
-setInterval(displayTime, 1000);
-
-let today = moment().format('YYYY-MM-DD');
-let now = moment().format("HH");
-
-// $("#00event").text(moment().isAfter(`${today} 21:00:00`));
-
-let calendarList = [{day:`${today}`,time:0,event:"abc"},{day:`${today}`,time:1,event:"cde"}];
-
-$("#calendar").on("click",".saveBtn",saveToLocal)
-
-function saveToLocal(event){
-    let input = $(event.target).parent().siblings().children().val();
-    let time = $(event.target).parent().siblings().children().attr("id");
-    
-}
-
-function getFromLocal(){
-
-}
-$("#0event").text(calendarList[0].event);
-
-setInterval(updateBgColor, 60000);
-
-updateBgColor();
-function updateBgColor(){
+//function to render calendar color
+function updateBgColor() {
     for (let i = 0; i < 24; i++) {
-        if(i<now){
-            $(`#${i}hh`).parent().css("background-color","#d3d3d3");
-        }else if(i>now){
-            $(`#${i}hh`).parent().css("background-color","#77dd77");
-        }else{
-            $(`#${i}hh`).parent().css("background-color","#ff6961");
+        if (i < now) {
+            $(`#${i}hh`).parent().css("background-color", "#d3d3d3");
+        } else if (i > now) {
+            $(`#${i}hh`).parent().css("background-color", "#77dd77");
+        } else {
+            $(`#${i}hh`).parent().css("background-color", "#ff6961");
         }
     }
-    
+
 }
+
+//call function retrieve event from storage and display to calendar
+getFromLocal();
+
+
+$("#calendar").on("click", ".saveBtn", saveToLocal)
+
+function saveToLocal(event) {
+    //trace from button, to find the value of corresponded text field.
+    let input = $(event.target).parent().siblings().children().val();
+    let stime = $(event.target).parent().siblings().children().attr("id").slice(0, -5);
+    let time = Number(stime);
+
+    if (isEventExist(time, input)) {
+        localStorage.setItem(`${today}`, JSON.stringify(calendarList));
+        getFromLocal();
+    } else {
+        calendarList.push({ "time": time, "event": input });
+        localStorage.setItem(`${today}`, JSON.stringify(calendarList));
+        getFromLocal();
+
+
+    }
+}
+
+function isEventExist(time, input) {
+    for (let i = 0; i < calendarList.length; i++) {
+        if (time == calendarList[i].time) {
+            calendarList[i].event = input;
+            return true;
+        }
+    }
+    return false;
+}
+
+function getFromLocal() {
+    calendarList = JSON.parse(localStorage.getItem(`${today}`));
+    console.log(calendarList)
+    if (calendarList == null) {
+        calendarList = [];
+    } else {
+        for (let i = 0; i < calendarList.length; i++) {
+            $(`#${calendarList[i].time}event`).text(calendarList[i].event);
+        }
+    }
+}
+
+
+
+
 
